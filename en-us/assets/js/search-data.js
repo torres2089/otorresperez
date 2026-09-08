cd96a004e8ab3cd94f10ec88f62c06036872b85b
torres2089 <oscartp353@gmail.com>
2026-09-08 21:12:17 +0000

    Deploying to gh-pages from @ torres2089/otorresperez@0e3329fb4ede32866de1f4cdc0b6a0b8dbc54558 🚀

--- en-us/assets/js/search-data.js
+++ en-us/assets/js/search-data.js
@@ -14,8 +14,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-sobre-mí",
-    title: "sobre mí",
+    id: "nav-about",
+    title: "about",
     section: "Navigation menu",
     handler: () => {
       window.location.href = "/otorresperez/en-us/";
