cb7e29c039f668e2a1d1555ddb16aeebd2ad942b
torres2089 <oscartp353@gmail.com>
2026-09-08 22:04:21 +0000

    Deploying to gh-pages from @ torres2089/otorresperez@b6aa9ede9ad8749778919bd0cc6f0a39bb79bfb8 🚀

--- en-us/assets/js/search-data.js
+++ en-us/assets/js/search-data.js
@@ -35,11 +35,11 @@ ninja.data = [{
             window.location.href = "/otorresperez/en-us/repositories/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "My complete résumé, structured for quick reading.",
           section: "Navigation menu",
           handler: () => {
-            window.location.href = "/otorresperez/en-us/cv/";
+            window.location.href = "/otorresperez/en-us/en-us/cv/";
           },
         },{id: "nav-teaching",
           title: "teaching",
